2b3ed8e5bcd1fe587057ce851fd534c785d46688
ProfessorProspector <thedoctorsoda@gmail.com>
2018-12-04 19:40:24 -0800

    Update

--- src/main/java/prospector/appetite/Appetite.java
+++ src/main/java/prospector/appetite/Appetite.java
@@ -6,19 +6,11 @@ import net.minecraft.item.Item;
 import net.minecraft.item.block.BlockItem;
 import net.minecraft.sortme.ItemGroup;
 import net.minecraft.util.registry.Registry;
-import prospector.appetite.block.BlockBerryBush;
 
 public class Appetite implements ModInitializer {
 	public static final String MOD_ID = "appetite";
 
-	public static final Block BLUEBERRY_BUSH;
-	public static final Block BLACKBERRY_BUSH;
-	public static final Block RASPBERRY_BUSH;
-
 	static {
-		BLUEBERRY_BUSH = register("blueberry_bush", new BlockBerryBush(), ItemGroup.DECORATIONS);
-		BLACKBERRY_BUSH = register("blackberry_bush", new BlockBerryBush(), ItemGroup.DECORATIONS);
-		RASPBERRY_BUSH = register("raspberry_bush", new BlockBerryBush(), ItemGroup.DECORATIONS);
 	}
 
 	private static Block register(String name, Block block, ItemGroup tab) {
