4c4ad565bab1c624d89cfe69af5ab4c1d89b5efd
ProfessorProspector <thedoctorsoda@gmail.com>
2018-12-16 17:29:50 -0800

    Update yarn + add icons

--- src/main/java/io/github/prospector/appetite/registry/AppetiteBlocks.java
+++ src/main/java/io/github/prospector/appetite/registry/AppetiteBlocks.java
@@ -1,22 +1,17 @@
 package io.github.prospector.appetite.registry;
 
-import io.github.prospector.appetite.block.CounterBlock;
+import io.github.prospector.appetite.Appetite;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.block.FabricBlockSettings;
 import net.minecraft.block.Block;
-import net.minecraft.block.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.block.BlockItem;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.registry.Registry;
-import io.github.prospector.appetite.Appetite;
 
 public class AppetiteBlocks implements ModInitializer {
-	public static final Block COUNTER = register("counter", new CounterBlock(FabricBlockSettings.create(Material.WOOD).setHardness(2.5F).setSoundGroup(BlockSoundGroup.WOOD).build()), ItemGroup.DECORATIONS);
 
 	public static Block register(String name, Block block, ItemGroup tab) {
-		Registry.register(Registry.BLOCKS, Appetite.MOD_ID + ":" + name, block);
+		Registry.register(Registry.BLOCK, Appetite.MOD_ID + ":" + name, block);
 		BlockItem item = new BlockItem(block, new Item.Settings().itemGroup(tab));
 		item.registerBlockItemMap(Item.BLOCK_ITEM_MAP, item);
 		AppetiteItems.register(name, item);
--- src/main/java/io/github/prospector/appetite/registry/AppetiteItems.java
+++ src/main/java/io/github/prospector/appetite/registry/AppetiteItems.java
@@ -1,14 +1,18 @@
 package io.github.prospector.appetite.registry;
 
-import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
-import net.minecraft.util.registry.Registry;
 import io.github.prospector.appetite.Appetite;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.FoodItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.registry.Registry;
 
 public class AppetiteItems implements ModInitializer {
 
+	public static final Item APPLE_PIE = register("apple_pie", new FoodItem(7, 0.3F, false, new Item.Settings().itemGroup(ItemGroup.FOOD)));
+
 	public static Item register(String name, Item item) {
-		Registry.register(Registry.ITEMS, Appetite.MOD_ID + ":" + name, item);
+		Registry.register(Registry.ITEM, Appetite.MOD_ID + ":" + name, item);
 		return item;
 	}
 
